02903bd0ccd2aafb998838bda1bb917e1f492dbb
Fazeel Hussain <Fhussain2644@conestogac.on.ca>
2021-11-24 15:45:48 -0500

    updated main

--- Project3_Ali_Fazeel_Milan/main.cpp
+++ Project3_Ali_Fazeel_Milan/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "rgb.h"
 
 using namespace std;
 
@@ -8,4 +9,41 @@ int main()
 	cout << "test" << endl;
 	//change
 	//made a change
+    rgb r;
+    int colour;
+    do {
+        cout << "Please select a colour to change to\n";
+        cout << "1. Blue\n2. Green\n3. Cyan\n4. Red\n5. Purple\n6. Yellow\n7. White\n8. to exit\n\n";
+        cin >> colour;
+
+        if (colour == 8) {
+            break;
+        }
+        else if (colour == 1) {
+            r.setColour(colour);
+        }
+        else if (colour == 2) {
+            r.setColour(colour);
+        }
+        else if (colour == 3) {
+            r.setColour(colour);
+        }
+        else if (colour == 4) {
+            r.setColour(colour);
+        }
+        else if (colour == 5) {
+            r.setColour(colour);
+        }
+        else if (colour == 6) {
+            r.setColour(colour);
+        }
+        else if (colour == 7) {
+            r.setColour(15);
+        }
+        else {
+            cout << "invalid entry please try again\n\n";
+        }
+
+
+    } while (true);
 }
