a1802b4d2cf53d3111727f8d930143b76c1cadd8
Fazeel Hussain <Fhussain2644@conestogac.on.ca>
2021-11-24 14:48:04 -0500

    lqc module complete

--- Project3_Ali_Fazeel_Milan/lqc.cpp
+++ Project3_Ali_Fazeel_Milan/lqc.cpp
@@ -15,7 +15,7 @@ public:
 	}
 	
 	// write to a file in set
-	void setPumpSpeed(int speed) {
+	void setPumpSpeed(string speed) {
 		pumpSpeed = speed;
 
 		std::ofstream file;
@@ -26,17 +26,37 @@ public:
 		file.close();
 	}
 
+	// if yo u set the pump speed, it should write to a file wth time stamp
+	//if u get the temprature, it should calculate the pump speed (using temprature) and write to the file wth time stamp
+
+
 	//read from a file in get
 	// ---> create a random num gen to get a random pump speed
-	string getPumpSpeed() {
-		std::ifstream file("pumpSpeedRead.txt");
-		
-		string line;
-		getline(file, line);
-		cout << line << endl;
-		pumpSpeed = line;
+	string getPumpSpeed(int cpuTemp, int gpuTemp) { //should call the calculate pump speed function (wich will also log after calculating) and then return the pump speed to the caller
+		int temp = cpuTemp + gpuTemp;
+		temp = temp / 2;
 
+		pumpSpeed = calcPumpSpeed(temp);
 		return pumpSpeed;
+	}
+
+	string calcPumpSpeed(int temp) {
+		if (150 > temp > 100) {
+			pumpSpeed = "1200";
+		}
+		else if (50 < temp < 100) {
+			pumpSpeed = "1000";
+		}
+		else if (200 > temp > 150) {
+			pumpSpeed = "1500";
+		}
+		else {
+			cout << "temprature level critical\n";
+		}
+		setPumpSpeed(pumpSpeed);
+		return pumpSpeed;
+
+
 	}
 
 	void pumpOn() {
